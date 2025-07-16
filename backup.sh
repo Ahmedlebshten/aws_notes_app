7dfdab90c91a6fecb715a2b9993487b77f9dbb0e
Cloud User <ec2-user@ip-172-31-18-129.ec2.internal>
2025-07-16 15:23:02 +0000

    secure backup.sh file

--- backup.sh
+++ backup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 TIMESTAMP=$(date +%F_%H-%M)
-mysqldump -u root -p12345 notesdb > /mariadb-backup/notes_backup_$TIMESTAMP.sql
+mysqldump notesdb > /mariadb-backup/notes_backup_$TIMESTAMP.sql
+
